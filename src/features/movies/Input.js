682eb993975a82477ce85c70cb33c7b00037c0a5
Aayushpant44 <aayushpant44@gmail.com>
2024-05-31 17:17:47 +0545

    change

--- src/features/movies/Input.js
+++ src/features/movies/Input.js
@@ -7,14 +7,15 @@ function Input() {
   const dispatch = useDispatch();
 
   const handleSubmit = () => {
+    
     dispatch(addMovie(text));
   };
   return (
     <div className="flex ">
       <input
         type="text"
-        className="w-full p-2 border border-gray-300 rounded-l-xl focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 capitalize"
-        placeholder="Enter your text here"
+        className="w-full p-2 border border-gray-300 rounded-l-xl focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 "
+        placeholder="Enter your movie name here"
         onChange={(e) => settext(e.target.value)}
       />
 
